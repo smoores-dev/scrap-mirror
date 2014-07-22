4708753d575700258bbae33f595749d66e808f57
Shane Moore <shanewmoore@gmail.com>
2014-07-21 19:20:29 -0700

    Fixed!

--- assets/js/draggable.js
+++ assets/js/draggable.js
@@ -4,23 +4,31 @@ $(document).ready(function() {
     revert: true,
     connectWith: '.column.normal',
     cursor: 'move',
-    items: '> :not(.add)'
+    items: '> :not(.add)',
+    stop: function(event, ui) {
+      var columnId = ui.item.data('columnid');
+      var newElement = '<article class="add">'+
+          '<form>'+
+          '<input type="hidden" name="columnid" value='+columnId+'></input>'+
+          '<input type="hidden" name="index" value="1"></input>'+
+          '<input class="add" type="text" name="content" placeholder="Add something new"></input>'+
+          '<input type="submit" style="visibility:hidden;"></input>'+
+          '</form>'+
+          '</article>'
+      if(ui.item.next().hasClass('add')) {
+        $(ui.item).before(newElement);
+      }
+      else {
+        $(ui.item).after(newElement);
+      }
+    }
   });
 
-  $('column.normal').on('mousedown',function() {
-    console.log("Clicked down");
-    $(this).next().remove();
-  }).on('mouseup', function() {
-    console.log("Unclick");
-    columnId = $(this).data('columnid')
-    $(this).after('<article class="add">'+
-      '<form>'+
-      '<input type="hidden" name="columnid" value='+columnId+'></input>'+
-      '<input type="hidden" name="index" value="1"></input>'+
-      '<input class="add" type="text" name="content" placeholder="Add something new"></input>'+
-      '<input type="submit" style="visibility:hidden;"></input>'+
-      '</form>'+
-      '</article>')
+  $('.column.normal > article:not(.add)').on('mousedown', function() {
+    $(this).on('mousemove', function() {
+      $(this).next().remove();
+      $(this).off('mousemove');
+    });
   });
 
 })
